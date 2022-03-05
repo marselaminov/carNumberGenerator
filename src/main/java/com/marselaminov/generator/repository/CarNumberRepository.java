a4ab3e21ef8ad14eb841e5802cba99b2b96952e9
marselaminov <Mylovea015to!>
2022-03-05 22:42:09 +0300

    поправлен интерфейс репозитория

--- src/main/java/com/marselaminov/generator/repository/CarNumberRepository.java
+++ src/main/java/com/marselaminov/generator/repository/CarNumberRepository.java
@@ -3,7 +3,9 @@ package com.marselaminov.generator.repository;
 import com.marselaminov.generator.model.CarNumber;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarNumberRepository extends JpaRepository<CarNumber, Long> {
     @Query(value ="select * from car_number order by id desc limit 1", nativeQuery = true)
     CarNumber getLastElementFromTable();
